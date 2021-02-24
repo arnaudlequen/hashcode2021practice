51c8381f77432f0a418eb2aade29a9ddb76d8abf
Phyronnaz <phyronnaz@gmail.com>
2021-02-24 12:41:48 -0800

    Add file read

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import sys
 
-FILE_NAME = "a_example.in"
+assert len(sys.argv) == 2, "Usage: main.py filename"
 
-def main():
-    f = open(FILE_NAME)
-    N, T2, T3, T4 = list(map(int, f.readline().split()))
+FILE_NAME = sys.argv[1]
+print("Reading " + FILE_NAME)
+
+with open(FILE_NAME) as file:
+    N, T2, T3, T4 = list(map(int, file.readline().split()))
 
     pizzas = []
 
     for _ in range(N):
-        pizzas.append(set(f.readline().split()[1:]))
+        pizzas.append(set(file.readline().split()[1:]))
 
     for p in pizzas:
         print(p)
-
-
-
-
-main()
