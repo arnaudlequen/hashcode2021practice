fa23da79d342094ec375a4f9611316cdec22df46
Arnaud Lequen <arnaud.lequen@ens-rennes.fr>
2021-02-24 21:39:21 +0100

    Nonocommito

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import sys
+
+FILE_NAME = "a_example.in"
+
 def main():
-    N, T2, T3, T4 = list(map(int, input().split()))
+    f = open(FILE_NAME)
+    N, T2, T3, T4 = list(map(int, f.readline().split()))
 
     pizzas = []
 
     for _ in range(N):
-        pizzas.append(set(input().split()[1:]))
+        pizzas.append(set(f.readline().split()[1:]))
 
     for p in pizzas:
         print(p)
