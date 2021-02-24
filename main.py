233ccdb94855677ef985688a75575398e2fe8bcb
Arnaud Lequen <arnaud.lequen@ens-rennes.fr>
2021-02-24 21:34:35 +0100

    Nonocommito

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("This is probably going to win the contest. Hands-down.")
-print("Hellow World")
+def main():
+    N, T2, T3, T4 = list(map(int, input().split()))
+
+    pizzas = []
+
+    for _ in range(N):
+        pizzas.append(set(input().split()[1:]))
+
+    for p in pizzas:
+        print(p)
+
+
+
+
+main()
